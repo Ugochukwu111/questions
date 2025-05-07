949d6354718d473a263017e1f1cd36e54cb6fb44
Pascal <pas111cal3@gmail.com>
2025-05-07 17:28:35 +0100

    made site reponsive and mobile side bar responsive

--- scripts/main.js
+++ scripts/main.js
@@ -1,3 +1,4 @@
+// animations for the main page site
 document.addEventListener("DOMContentLoaded", function () {
   // Get all the children of the footer
   const footerElements = document.querySelectorAll("footer > *");
@@ -19,4 +20,34 @@ document.addEventListener("DOMContentLoaded", function () {
   footerElements.forEach(element => {
     observer.observe(element);
   });
+});
+
+// MOBILE NAVIGATION JS
+const SidebarBtn = document.querySelector(".sidebar-btn");
+SidebarBtn.addEventListener("click",  ()=>{
+  let closeIcon = document.querySelector(".close-icon ");
+  let showIcon = document.querySelector(".show-icon ");
+  let sidebar = document.querySelector(".sidebar");
+  let sidebarLabel = sidebar.getAttribute('aria-label');
+
+  if (sidebarLabel === "sidebar closed") {
+    sidebar.setAttribute('aria-label', 'sidebar opened');
+    showIcon.classList.add("hide-icon");
+    closeIcon.classList.remove("hide-icon");
+    console.log(sidebar)
+
+    sidebar.classList.add("show-sidebar");
+
+    console.log(sidebar.getAttribute('aria-label'));
+  }else{
+    sidebar.setAttribute('aria-label', 'sidebar closed');
+    
+    sidebar.classList.remove("show-sidebar");
+
+    closeIcon.classList.add("hide-icon");
+    showIcon.classList.remove("hide-icon");
+
+    console.log(sidebar.getAttribute('aria-label'));
+  }
+
 });
